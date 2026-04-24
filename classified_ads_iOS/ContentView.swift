0c80ff8394f191db1f383421407ffad40c419196
Amit Singh Raghuvanshi <asraghuvanshi52@gmail.com>
2026-04-24 23:38:11 +0530

    Initial Commit

---
+++ classified_ads_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  classified_ads_iOS
+//
+//  Created by iOS Developer on 24/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ classified_ads_iOS/classified_ads_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  classified_ads_iOSApp.swift
+//  classified_ads_iOS
+//
+//  Created by iOS Developer on 24/04/26.
+//
+
+import SwiftUI
+
+@main
+struct classified_ads_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
